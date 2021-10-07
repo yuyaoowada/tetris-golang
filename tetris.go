b516caa882f57aeafc121562a59928bc11aa4666
yuyaoowada <yuyaoowada@gmail.com>
2021-10-07 22:14:52 +0900

    add method move,rotate,erase

--- tetris.go
+++ tetris.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
 	"os/exec"
 	"time"
+
+	"github.com/mattn/go-tty"
 )
 
 // 定数定義
@@ -142,7 +146,7 @@ func drawDisp() {
 			}
 
 			if disp != 0 {
-				dispStr += "□"
+				dispStr += "□ "
 			} else {
 				dispStr += "  "
 			}
@@ -163,25 +167,6 @@ func drawDisp() {
 
 	// まとめて表示
 	fmt.Printf("%s", dispStr)
-}
-
-func mainLoop() {
-	// ループミリ秒
-	var loopms time.Duration = 33
-	// タイマ作成
-	timer := time.NewTimer(loopms * time.Millisecond)
-
-	// メインループ
-	for {
-		select {
-		case <-timer.C:
-			timer = time.NewTimer(loopms * time.Millisecond)
-			// fmt.Printf(".")
-			// 描画
-			drawDisp()
-			break
-		}
-	}
 }
 
 // すでに積んであるブロックと現在のブロックの当たり判定
@@ -221,6 +206,256 @@ func intersectBlockUpper() int {
 		}
 	}
 	return 0
+}
+
+// 降っているブロックからフィールドへ
+func nowBlockToField() {
+	for innerY := 0; innerY < 4; innerY++ {
+		for innerX := 0; innerX < 4; innerX++ {
+			x := nowBlockX + innerX
+			y := nowBlockY + innerY
+			if (0 <= x && x < dispW) && (0 <= y && y < dispH) {
+				if nowBlock[innerY][innerX] != 0 {
+					field[y][x] = nowBlock[innerY][innerX]
+				}
+			}
+		}
+	}
+}
+
+// ブロックが消える処理
+func eraseBlock() {
+	// 空白がある行をテンポラリにコピーする
+	var tmpField [dispH][dispW]int
+
+	tmpY := dispH - 1
+	for y := dispH - 1; y >= 0; y-- {
+		blank := 0 // 空白ブロックがあるか
+
+		// 一行処理
+		for x := 0; x < dispW; x++ {
+			if field[y][x] == 0 {
+				// 空白を見つけた
+				blank = 1
+				break
+			}
+		}
+
+		// テンポラリにコピー
+		if blank != 0 {
+			for x := 0; x < dispW; x++ {
+				tmpField[tmpY][x] = field[y][x]
+			}
+			tmpY--
+		}
+	}
+
+	// テンポラリからフィールドに移す
+	for y := 0; y < dispH; y++ {
+		for x := 0; x < dispW; x++ {
+			field[y][x] = tmpField[y][x]
+		}
+	}
+}
+
+// ブロックの回転
+func rotateBlockL() {
+	// テンポラリ
+	var tmpBlock [4][4]int
+	var srcX, srcY, dstX, dstY int
+
+	// 今降っているブロックテンポラリ
+	srcY = 0
+	dstX = 0
+	for srcY < 4 {
+		srcX = 0
+		dstY = 3
+		for srcX < 4 {
+			tmpBlock[dstY][dstX] = nowBlock[srcY][srcX]
+			srcX++
+			dstY--
+		}
+		srcY++
+		dstX++
+	}
+
+	// テンポラリ→今降っているブロック
+	for y := 0; y < 4; y++ {
+		for x := 0; x < 4; x++ {
+			nowBlock[y][x] = tmpBlock[y][x]
+		}
+	}
+}
+
+// 初期化
+func initGame() {
+	// 乱数初期化
+	rand.Seed(time.Now().UnixNano())
+
+	// フィールド初期化
+	for y := 0; y < dispH; y++ {
+		for x := 0; x < dispW; x++ {
+			field[y][x] = 0
+		}
+	}
+
+	// 降っているテトリス初期化
+	for y := 0; y < 4; y++ {
+		for x := 0; x < 4; x++ {
+			nowBlock[y][x] = 0
+		}
+	}
+	nowBlockX = 0
+	nowBlockY = 0
+	speed = initSpeed
+	speedCnt = speed
+	waitBlockCnt = waitBlockFPS
+
+	// ゲーム状態初期化
+	gameStatus = 1
+}
+
+// ゲーム処理
+func execGame(str string) {
+	if str == "q" {
+		// 終了
+		os.Exit(0)
+	}
+
+	switch gameStatus {
+	case 1: // 降るテトリス選定
+		{
+			// ブロック決定
+			bn := rand.Intn(len(block))
+			// ブロックコピー
+			for y := 0; y < 4; y++ {
+				for x := 0; x < 4; x++ {
+					nowBlock[y][x] = block[bn][y][x]
+				}
+			}
+			// 位置の初期化
+			nowBlockX = dispW/2 - 2
+			nowBlockY = -4
+			// 降っているテトリスが止まるカウント
+			waitBlockCnt = waitBlockFPS
+		}
+		gameStatus++
+		break
+	case 2: // 降っている状態
+		{
+			if str == " " { // 回転
+				rotateBlockL()
+				waitBlockCnt = waitBlockFPS
+			}
+			if str == "a" { // 左
+				if intersectBlock(-1, 0) == 0 {
+					nowBlockX--
+				}
+			}
+			if str == "d" { // 右
+				if intersectBlock(1, 0) == 0 {
+					nowBlockX++
+				}
+			}
+			if str == "s" { // 下
+				if intersectBlock(0, 1) == 0 {
+					nowBlockY++
+				}
+			}
+			if str == "w" { // 上
+				for {
+					if intersectBlock(0, 1) != 0 {
+						break
+					}
+					nowBlockY++
+					waitBlockCnt = 1
+				}
+			}
+
+			if speedCnt <= 0 {
+				if intersectBlock(0, 1) == 0 {
+					nowBlockY++
+				}
+				speedCnt = speed
+			}
+			speedCnt--
+
+			// 回転して重なるなら戻す
+			for i := 0; i < 4; i++ {
+				if intersectBlock(0, 0) != 0 {
+					if intersectBlock(-1, 0) == 0 {
+						nowBlockX--
+						break
+					}
+					if intersectBlock(1, 0) == 0 {
+						nowBlockX++
+						break
+					}
+					nowBlockY--
+				}
+			}
+			// ブロックが着地しているか
+			if intersectBlock(0, 1) != 0 {
+				waitBlockCnt--
+				if waitBlockCnt <= 0 {
+					// 位置指定
+					nowBlockToField()
+					eraseBlock()
+					// ゲームオーバー判定
+					if intersectBlockUpper() != 0 {
+						// ゲームオーバー
+						gameStatus = 3
+					} else {
+						// 継続
+						gameStatus = 1
+					}
+				}
+			}
+		}
+		break
+	case 3: //ゲームオーバー
+		fmt.Printf("GameOver.\n")
+		os.Exit(0)
+		break
+	}
+}
+
+// メインループ
+func mainLoop() {
+	// ループミリ秒
+	var loopms time.Duration = 33
+	// タイマ作成
+	timer := time.NewTimer(loopms * time.Millisecond)
+	// 初期化
+	initGame()
+
+	tty, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tty.Close()
+
+	// メインループ
+	for {
+		select {
+		case <-timer.C:
+			timer = time.NewTimer(loopms * time.Millisecond)
+			// 描画
+			drawDisp()
+			// ゲーム処理
+			for {
+				r, err := tty.ReadRune()
+				if err != nil {
+					log.Fatal(err)
+				}
+				if string(r) != "" {
+					// 入力によって操作
+					execGame(string(r))
+				}
+				break
+			}
+		}
+	}
 }
 
 func main() {
